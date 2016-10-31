5c1b9a4b1a72ee4a3bb375a21ffdb11b0ea4dd56
Helen <hso.sh@mysummitps.org>
2016-10-31 21:26:49 +0000

    Created color changing checkpoint

---
+++ changer.js
@@ -0,0 +1,10 @@
+// Creates variables and saves DOM elements to each
+var newBGcolor = document.getElementById("newBGColor");
+var colorChangeButton = document.getElementById("colorChangeButton");
+
+/* Event Listener for button.
+   Sets the body's background color to the user provided value
+*/
+colorChangeButton.addEventListener("click", function (){
+    document.body.style.backgroundColor = newBGcolor.value;
+})
