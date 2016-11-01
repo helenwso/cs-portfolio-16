c15fb4618839504cad6b57f01d313b6a3b290a11
Helen <hso.sh@mysummitps.org>
2016-11-01 22:16:14 +0000

    update

---
+++ calculator.js
@@ -0,0 +1,10 @@
+// Creates variables and saves DOM elements to each
+var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
+var dropdown = document.getElementById("dropdown");
+var magic = document.getElementById("magic");
+
+/* Event listener for button
+   Does operation */
+  
+magic.addEventListener ("click", magic);
