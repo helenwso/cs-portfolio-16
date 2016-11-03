54bb665bce9e26476cf8b77892ffefc6a0d61f5b
Helen <hso.sh@mysummitps.org>
2016-11-03 02:56:17 +0000

    made calc actually work

--- calculator.js
+++ calculator.js
@@ -2,9 +2,22 @@
 var input1 = document.getElementById("input1");
 var input2 = document.getElementById("input2");
 var dropdown = document.getElementById("dropdown");
-var magic = document.getElementById("magic");
+var answerButton = document.getElementById("answerButton");
+var answer;
+var display = document.getElementById("display");
 
 /* Event listener for button
    Does operation */
-  
-magic.addEventListener ("click", magic);
+
+answerButton.addEventListener("click", getAnswer);
+
+function getAnswer () {
+    if (dropdown.value == "add") {
+        answer = parseInt (input1.value, 10) + parseInt(input2.value, 10);
+        display.innerHTML = answer;
+    }
+    else if (dropdown.value == "multiply") {
+        answer = parseInt(input1.value, 10) * parseInt(input2.value, 10);
+        display.innerHTML = answer;
+    }
+}
--- changer.js
+++ changer.js
@@ -5,6 +5,7 @@ var colorChangeButton = document.getElementById("colorChangeButton");
 /* Event Listener for button.
    Sets the body's background color to the user provided value
 */
-colorChangeButton.addEventListener("click", function (){
+colorChangeButton.addEventListener("click", function myFunc (){
     document.body.style.backgroundColor = newBGcolor.value;
-})
+} )
+
