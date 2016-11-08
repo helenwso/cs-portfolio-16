ee961109e34eec76138300503ff97edde0ccffbb
Helen <hso.sh@mysummitps.org>
2016-11-08 03:56:16 +0000

    made the to-do list actually workgit add -Agit add -A

--- todo.js
+++ todo.js
@@ -3,12 +3,23 @@ var todoInput = document.getElementById("todoInput");
 
 // dropdown for choosing CSS styling
 var dropdown = document.getElementById("dropdown");
-
-// button the user clicks to add to-do
-var addButton = document.getElementById("addButton");
-
 // div where the user's to-do is displayed
 var todoDisplayDiv = document.getElementById("todoDisplayDiv");
+
+var addButton = document.getElementById("addButton");
+// button the user clicks to add to-do
+
+addButton.addEventListener("click", function (){
+    var addTask = "<div style = 'color" + color +"'>" + todoInput.value + "</div>";
+    todoDisplayDiv.innerHTML = todoDisplayDiv.innerHTML + addTask;
+    
+});
+
+todoDisplayDiv.addEventListener("click", function (evt){
+        var minusTask = evt. target;
+        minusTask.parentNode.removeChild (minusTask);
+    });
+
 
 // div where the user's completed tasks are displayed
 var completedDisplayDiv = document.getElementById("completedDisplayDiv");
@@ -17,10 +28,10 @@ var display;
 
 /* Event listener for addButton
    Adds items to list */
-addButton.addEventListener("click", function evt (){
-    todoDisplayDiv.innerHTML = todoInput.value;
-//    if (todoInput) has text, then put it into tododisplaydiv within p tag
-//else: do nothing
-    //if (dropdown.value =
-    // "black"){display.innerHTML = display
-})
+// addButton.addEventListener("click", function thing (evt){
+//     todoDisplayDiv.innerHTML = todoInput.value;
+// //    if (todoInput) has text, then put it into tododisplaydiv within p tag
+// //else: do nothing
+//     //if (dropdown.value =
+//     // "black"){display.innerHTML = display
+// })
