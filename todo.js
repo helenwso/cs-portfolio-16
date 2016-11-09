f6d8599186b8718c9ff8db38633fe3ef7ed505a7
Helen <hso.sh@mysummitps.org>
2016-11-09 23:29:51 +0000

    THE TODO LIST ACTUALLY DOES COLORS AND STRIKE AND DELETE AND YAYYYY

--- todo.js
+++ todo.js
@@ -9,40 +9,58 @@ var todoDisplayDiv = document.getElementById("todoDisplayDiv");
 var addButton = document.getElementById("addButton");
 // button the user clicks to add to-do
 
-addButton.addEventListener("click", function (){
-    
+var strikeTask;
 
-    var addBlackTask = "<p style ='color:black'>" + todoInput.value + "</p>";
-    
+var strikeThrough = document.getElementsByClassName("strikeThrough")
+
+addButton.addEventListener("click", function (){
+
+    var addBlackTask = "<div style ='color:black'>" + todoInput.value + "</div>";
     if (dropdown.value == "black") {
        todoDisplayDiv.innerHTML = todoDisplayDiv.innerHTML + addBlackTask; 
     }
 
-    var addRedTask = "<p style ='color:red'>" + todoInput.value + "</p>";
-    
+    var addRedTask = "<div style ='color:red'>" + todoInput.value + "</div>";
     if (dropdown.value == "red") {
        todoDisplayDiv.innerHTML = todoDisplayDiv.innerHTML + addRedTask; 
     }
     
-    var addOrangeTask = "<p style ='color:orange'>" + todoInput.value + "</p>";
-    
+    var addOrangeTask = "<div style ='color:orange'>" + todoInput.value + "</div>";
     if (dropdown.value == "orange") {
        todoDisplayDiv.innerHTML = todoDisplayDiv.innerHTML + addOrangeTask; 
     }    
     
-    var addGreenTask = "<p style ='color:green'>" + todoInput.value + "</p>";
-    
+    var addGreenTask = "<div style ='color:green'>" + todoInput.value + "</div>";
     if (dropdown.value == "green") {
        todoDisplayDiv.innerHTML = todoDisplayDiv.innerHTML + addGreenTask; 
     }    
     
-    var addBlueTask = "<p style ='color:blue'>" + todoInput.value + "</p>";
-    
+    var addBlueTask = "<div style ='color:blue'>" + todoInput.value + "</div>";
     if (dropdown.value == "blue") {
        todoDisplayDiv.innerHTML = todoDisplayDiv.innerHTML + addBlueTask; 
     }            
+    
+    var addPurpleTask = "<div style ='color:purple'>" + todoInput.value + "</div>";
+    if (dropdown.value == "purple") {
+       todoDisplayDiv.innerHTML = todoDisplayDiv.innerHTML + addPurpleTask; 
+    }              
 });
+    
 
+todoDisplayDiv.addEventListener("click", function (evt){
+     var strikeTask = evt. target;
+     
+     if (strikeTask.style.textDecoration === "line-through"){
+         strikeTask.parentNode.removeChild(strikeTask);
+     }
+     else{
+         strikeTask.style.textDecoration = "line-through";
+     }
+});   
+// todoDisplayDiv.addEventListener("click", function (evt){
+//         var minusTask = evt. target;
+//         minusTask.parentNode.removeChild (minusTask);
+//     });
 
 // div where the user's completed tasks are displayed
 var completedDisplayDiv = document.getElementById("completedDisplayDiv");
